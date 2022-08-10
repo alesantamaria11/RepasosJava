b9caac9cbac32df950213b422903963bdc402567
alesantamaria11 <alesantamariadh@gmail.com>
2022-08-10 17:44:02 -0300

    primer commit

---
+++ project2/src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+/**
+ * @author alejandrasantamaria
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        //operadores artimeticos
+
+        //+ - / *
+        int num1 = 5;
+        int num2 = 2;
+
+        int r1 = num1 * num2;
+
+        System.out.println(r1);
+
+
+        //Funciones
+
+    }
+
+}
